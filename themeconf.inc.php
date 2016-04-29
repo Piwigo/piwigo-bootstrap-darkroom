7217d88e53db7b42669f6983807f3adc85ca6fb1
Thomas Kuther <github@kuther.net>
2016-04-29 09:12:53 +0200

    add event handler that returns all pictures

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -18,8 +18,8 @@ $themeconf = array(
 
 // needed for the navigation carousel in picture.tpl
 // borrowed from https://github.com/ThomasDaheim/piwigo-stuff/tree/master/picturethumbs
-add_event_handler('loc_end_picture', 'add_thumbnails_to_template');
-function add_thumbnails_to_template()
+add_event_handler('loc_end_picture', 'get_all_thumbnails_in_category');
+function get_all_thumbnails_in_category()
 {
   global $template, $conf, $user, $page;
   
@@ -74,6 +74,66 @@ function add_thumbnails_to_template()
       ) );
   $tpl_thumbnails_var = trigger_change('loc_end_index_thumbnails', $tpl_thumbnails_var, $pictures);
   $template->assign('thumbnails', $tpl_thumbnails_var);
+
+  unset($tpl_thumbnails_var, $pictures);
+}
+
+add_event_handler('loc_begin_picture', 'get_all_pictures_in_category');
+function get_all_pictures_in_category()
+{
+  global $template, $conf, $user, $page;
+  
+  // select all pictures for this category
+  $query = '
+  SELECT *
+    FROM '.IMAGES_TABLE.'
+    WHERE id IN ('.implode(',', $page['items']).')
+    ORDER BY FIELD(id, '.implode(',', $page['items']).')
+  ;';
+
+  $result = pwg_query($query);
+  
+  $pictures = array();
+  while ($row = pwg_db_fetch_assoc($result))
+  {
+    $pictures[] = $row;
+  }
+  
+  trigger_notify('loc_begin_index_thumbnails', $pictures);
+  $tpl_thumbnails_var = array();
+
+  foreach ($pictures as $row)
+  {    
+    $url = duplicate_picture_url(
+      array(
+        'image_id' => $row['id'],
+        'image_file' => $row['file'],
+        ),
+      array('start')
+      );
+
+    $name = render_element_name($row);
+    $desc = render_element_description($row, 'main_page_element_description');
+    
+    $tpl_var = array_merge( $row, array(
+      'NAME' => $name,
+      'TN_ALT' => htmlspecialchars(strip_tags($name)),
+      'TN_TITLE' => get_thumbnail_title($row, $name, $desc),
+      'URL' => $url,
+      'DESCRIPTION' => $desc,
+      'src_image' => new SrcImage($row),
+    ) );
+    
+    $tpl_thumbnails_var[] = $tpl_var;
+  }
+  
+  $template->assign( array(
+    'derivative_params' => trigger_change('get_index_derivative_params', ImageStdParams::get_by_type( pwg_get_session_var('index_deriv', IMG_LARGE) ) ),
+    'maxRequests' =>$conf['max_requests'],
+    'SHOW_THUMBNAIL_CAPTION' =>$conf['show_thumbnail_caption'],
+      ) );
+  $tpl_thumbnails_var = trigger_change('loc_end_index_thumbnails', $tpl_thumbnails_var, $pictures);
+  $template->assign('pictures', $tpl_thumbnails_var);
 
   unset($tpl_thumbnails_var, $pictures);
 }
