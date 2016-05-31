21a022bbc614b78fe01958120770ecba731f0a77
Thomas Kuther <github@kuther.net>
2016-05-31 15:59:34 +0200

    register supported video types

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -104,5 +104,9 @@ function get_all_thumbnails_in_category()
   unset($tpl_thumbnails_var, $pictures);
 }
 
+// register video files
+$video_ext = array('mp4','m4v');
+$conf['file_ext'] = array_merge ($conf['file_ext'], $video_ext, array_map('strtoupper', $video_ext));
+
 $controller = new \BootstrapDarkroom\ThemeController();
 $controller->init();
