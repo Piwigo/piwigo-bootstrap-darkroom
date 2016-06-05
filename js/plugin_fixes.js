b9dfb2aa84975955955d6c686fdfbfbc6ccb84fc
Thomas Kuther <github@kuther.net>
2016-06-05 21:33:17 +0200

    Fix #4, Batch Downloader plugin failed when download permission is set to unique derivative only

--- js/plugin_fixes.js
+++ js/plugin_fixes.js
@@ -18,28 +18,32 @@
 
 /* Batch Downloader START */
 $(document).ready(function() {
-  $('#batchDownloadLink').closest('li').addClass('dropdown');
-  $('#batchDownloadLink').addClass('dropdown-toggle').removeClass('pwg-state-default pwg-button').attr('data-toggle', 'dropdown').attr('href', '#');
-  $('#batchDownloadLink .pwg-button-text').after('<span class="caret"></span>');
-  $('#batchDownloadBox').changeElementType('ul');
-  $('#batchDownloadBox a').wrap('<li></li>');
-  $('#batchDownloadBox').attr('role', 'menu').attr('style', '');
-  $('#batchDownloadBox .switchBoxTitle').changeElementType('li');
-  $('#batchDownloadBox .switchBoxTitle').addClass('dropdown-header').removeClass('switchBoxTitle');
-  $('#batchDownloadBox br').remove();
-  $('#batchDownloadBox').addClass('dropdown-menu').removeClass('switchBox');
-  $('.navbar-main dt:contains("Downloads")').next('dd').appendTo($('.navbar-main dt:contains("Downloads")'));
-  $('.navbar-main dt:contains("Downloads")').wrap('<li></li>');
-  $('.navbar-main dt:contains("Downloads")').closest('li').addClass('dropdown').attr('id', 'bd_downloads');
-  $('.navbar-main dt:contains("Downloads")').changeElementType('a');
-  $('#bd_downloads>a').addClass('dropdown-toggle').attr('data-toggle', 'dropdown').append('<span class="caret"></span>');
-  $('#bd_downloads dd ul').appendTo('#bd_downloads');
-  $('#bd_downloads dd').remove();
-  $('#bd_downloads ul').addClass('dropdown-menu');
-  $('#bd_downloads ul').find('span').each(function() { $(this).appendTo($(this).prev('a')); });
+  if ($('#batchDownloadLink').next('div#batchDownloadBox').length > 0) {
+    $('#batchDownloadLink').closest('li').addClass('dropdown');
+    $('#batchDownloadLink').addClass('dropdown-toggle').removeClass('pwg-state-default pwg-button').attr('data-toggle', 'dropdown').attr('href', '#');
+    $('#batchDownloadLink .pwg-button-text').after('<span class="caret"></span>');
+    $('#batchDownloadBox').changeElementType('ul');
+    $('#batchDownloadBox a').wrap('<li></li>');
+    $('#batchDownloadBox').attr('role', 'menu').attr('style', '');
+    $('#batchDownloadBox .switchBoxTitle').changeElementType('li');
+    $('#batchDownloadBox .switchBoxTitle').addClass('dropdown-header').removeClass('switchBoxTitle');
+    $('#batchDownloadBox br').remove();
+    $('#batchDownloadBox').addClass('dropdown-menu').removeClass('switchBox');
+    $('.navbar-main dt:contains("Downloads")').next('dd').appendTo($('.navbar-main dt:contains("Downloads")'));
+    $('.navbar-main dt:contains("Downloads")').wrap('<li></li>');
+    $('.navbar-main dt:contains("Downloads")').closest('li').addClass('dropdown').attr('id', 'bd_downloads');
+    $('.navbar-main dt:contains("Downloads")').changeElementType('a');
+    $('#bd_downloads>a').addClass('dropdown-toggle').attr('data-toggle', 'dropdown').append('<span class="caret"></span>');
+    $('#bd_downloads dd ul').appendTo('#bd_downloads');
+    $('#bd_downloads dd').remove();
+    $('#bd_downloads ul').addClass('dropdown-menu');
+    $('#bd_downloads ul').find('span').each(function() { $(this).appendTo($(this).prev('a')); });
+  }
 });
 $(window).load(function() {
-  $('#batchDownloadLink').off().on('click', function() { $('#downloadSizeLink').dropdown() });
+  if ($('#batchDownloadLink').next('ul#batchDownloadBox').length > 0) {
+    $('#batchDownloadLink').off().on('click', function() { $('#downloadSizeLink').dropdown() });
+  }
 });
 /* Batch Downloader END */
 
