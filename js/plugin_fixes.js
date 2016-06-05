0a1a5e791ca72cf75c5d8273269dc7a1178930e6
Thomas Kuther <github@kuther.net>
2016-06-05 16:25:12 +0200

    more fixes for batchdownloader plugin

--- js/plugin_fixes.js
+++ js/plugin_fixes.js
@@ -28,6 +28,15 @@ $(document).ready(function() {
   $('#batchDownloadBox .switchBoxTitle').addClass('dropdown-header').removeClass('switchBoxTitle');
   $('#batchDownloadBox br').remove();
   $('#batchDownloadBox').addClass('dropdown-menu').removeClass('switchBox');
+  $('.navbar-main dt:contains("Downloads")').next('dd').appendTo($('.navbar-main dt:contains("Downloads")'));
+  $('.navbar-main dt:contains("Downloads")').wrap('<li></li>');
+  $('.navbar-main dt:contains("Downloads")').closest('li').addClass('dropdown').attr('id', 'bd_downloads');
+  $('.navbar-main dt:contains("Downloads")').changeElementType('a');
+  $('#bd_downloads>a').addClass('dropdown-toggle').attr('data-toggle', 'dropdown').append('<span class="caret"></span>');
+  $('#bd_downloads dd ul').appendTo('#bd_downloads');
+  $('#bd_downloads dd').remove();
+  $('#bd_downloads ul').addClass('dropdown-menu');
+  $('#bd_downloads ul').find('span').each(function() { $(this).appendTo($(this).prev('a')); });
 });
 $(window).load(function() {
   $('#batchDownloadLink').off().on('click', function() { $('#downloadSizeLink').dropdown() });
