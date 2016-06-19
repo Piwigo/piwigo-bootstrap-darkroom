34316b0ab26f9e403b388d3573c8eaf53a7a6793
Thomas Kuther <github@kuther.net>
2016-06-19 18:40:47 +0200

    run metadata code only if enabled in config (scans each file in album, so..)

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -68,19 +68,23 @@ function get_all_thumbnails_in_category()
   
   $tpl_thumbnails_var = array();
 
-  if (array_key_exists('bootstrap_darkroom_ps_exif_mapping', $conf)) {
-    $exif_mapping = $conf['bootstrap_darkroom_ps_exif_mapping'];
-  } else {
-    $exif_mapping = array(
-      'date_creation' => 'DateTimeOriginal',
-      'make'          => 'Make',
-      'model'         => 'Model',
-      'lens'          => 'UndefinedTag:0xA434',
-      'shutter_speed' => 'ExposureTime',
-      'iso'           => 'ISOSpeedRatings',
-      'apperture'     => 'FNumber',
-      'focal_length'  => 'FocalLength',
-    );
+  $theme_config = $template->get_template_vars('theme_config_extra');
+
+  if ($theme_config->photoswipe_metadata) {
+    if (array_key_exists('bootstrap_darkroom_ps_exif_mapping', $conf)) {
+      $exif_mapping = $conf['bootstrap_darkroom_ps_exif_mapping'];
+    } else {
+      $exif_mapping = array(
+        'date_creation' => 'DateTimeOriginal',
+        'make'          => 'Make',
+        'model'         => 'Model',
+        'lens'          => 'UndefinedTag:0xA434',
+        'shutter_speed' => 'ExposureTime',
+        'iso'           => 'ISOSpeedRatings',
+        'apperture'     => 'FNumber',
+        'focal_length'  => 'FocalLength',
+      );
+    }
   }
 
   foreach ($pictures as $row)
@@ -104,13 +108,18 @@ function get_all_thumbnails_in_category()
       'src_image' => new SrcImage($row),
       'SIZE' => $row['width'].'x'.$row['height'],
       'PATH' => $row['path'],
-      'EXIF' => get_exif_data($row['path'], $exif_mapping),
     ) );
-    
-    //optional replacements
-    if (array_key_exists('bootstrap_darkroom_ps_exif_replacements', $conf)) {
-      foreach ($conf['bootstrap_darkroom_ps_exif_replacements'] as $tag => $replacement) {
-        $tpl_var['EXIF'][$tag] = str_replace($replacement[0], $replacement[1], $tpl_var['EXIF'][$tag]);
+
+    if ($theme_config->photoswipe_metadata) {
+      $tpl_var = array_merge( $tpl_var, array (
+        'EXIF' => get_exif_data($row['path'], $exif_mapping),
+      ) );
+
+      //optional replacements
+      if (array_key_exists('bootstrap_darkroom_ps_exif_replacements', $conf)) {
+        foreach ($conf['bootstrap_darkroom_ps_exif_replacements'] as $tag => $replacement) {
+          $tpl_var['EXIF'][$tag] = str_replace($replacement[0], $replacement[1], $tpl_var['EXIF'][$tag]);
+        }
       }
     }
  
