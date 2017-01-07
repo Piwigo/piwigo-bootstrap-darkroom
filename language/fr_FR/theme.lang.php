868b03007e5c341ada07141b9b72cb8547aa45ae
Thomas Kuther <github@kuther.net>
2017-01-07 08:39:08 +0100

    add french translation, thanks Guillaume.

---
+++ language/fr_FR/theme.lang.php
@@ -0,0 +1,64 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Enabled'] = 'Activé';
+$lang['Disabled'] = 'Désactivé';
+$lang['Color theme'] = 'Thème';
+$lang['Bootstrap Darkroom\'s custom dark color theme'] = 'Bootstrap Darkroom custom de couleur sombre';
+$lang['A color theme from'] = 'Un thème de';
+$lang['Bootstrap\'s default theme'] = 'Le thème standard de Bootstrap';
+$lang['No color theme'] = 'Aucun thème';
+$lang['Bootswatch theme'] = 'Thème Bootswatch';
+$lang['Bootstrap theme'] = 'Thème Bootstrap';
+$lang['Enable the slick carousel below the main image on the picture page'] = 'Active le Slick Carousel sous l\'image de la page image';
+$lang['lazyLoad method'] = 'Méthode lazyLoad';
+$lang['will load the image as soon as you slide to it'] = 'chargera l\'image dès que vous glisserez sur elle';
+$lang['loads all images one after another when the page loads (use carefully!)'] = 'charge les images l\'une après l\'autre au chargement de la page (à utiliser avec prudence !)';
+$lang['Infinite looping'] = 'Boucle infinie';
+$lang['Endlessly scroll through album images'] = 'Permet le défilement infini à travers les photos de l\'albumm';
+$lang['Center mode'] = 'Mode de centrage';
+$lang['Display the currently selected image in the middle. Works best with infinite looping enabled.'] = 'Affiche l\'image actuelle dans le centre des carrousels. Fonctionne mieux avec la répétition sans fin activée.';
+$lang['Enable PhotoSwipe fullscreen slideshow. Disable if you prefer to use Plugins like Fotorama or Piwigo\'s default slideshow.'] = 'Activer le diaporama PhotoSwipe en plein écran. Désactiver si vous préférez des plugins comme Fotorama ou le diaporama de Piwigo.';
+$lang['Autoplay interval'] = 'Intervalle de lecture automatique';
+$lang['milliseconds'] = 'millisecondes';
+$lang['Show a jumbotron banner on the categroy pages.'] = 'Afficher un bandeau Jumbotron sur les pages de catégories.';
+$lang['Picture page display'] = 'Affichage de la page image';
+$lang['Tabs below the image'] = 'Onglets sous l\'image';
+$lang['Sidebar (like Boostrap Default)'] = 'Sidebar (comme Boostrap Default)';
+$lang['Picture info display position'] = 'Position des informations de l\'image';
+$lang['Note: on mobile devices the Tabs are forced, because the Sidebar button would overlay the main picture.'] = 'Remarque: sur les appareils mobiles, les onglets sont forcés car le bouton de la barre latérale recouvrait l\'image principale.';
+$lang['Thumbnail Link Target'] = 'Cible des miniatures';
+$lang['Link thumbnail to'] = 'Cliquer sur les miniatures ouvre';
+$lang['Picture details page'] = 'Page détaillée de l\'image';
+$lang['PhotoSwipe Slideshow'] = 'Diaporama PhotoSwipe';
+$lang['Photoswipe Slideshow (Mobile devices only)'] = 'Diaporama PhotoSwipe (uniquement sur les périphériques mobiles)';
+$lang['Category page display'] = 'Affichage de la page des catégories';
+$lang['Display categories as Bootstrap media wells'] = 'Afficher les catégories comme Bootstrap Media Wells';
+$lang['Never'] = 'Jamais';
+$lang['Always'] = 'Toujours';
+$lang['On mobile devices only'] = 'Sur périphériques mobiles uniquementn';
+$lang['This will display categories as media wells with squared thumbnails, similar to the smartpocket mobile theme.'] = 'Cela affichera les catégories avec des miniatures carrées, similaire au thème mobile smartpocket';
+$lang['Show basic EXIF metadata'] = 'Afficher les métadonnées EXIF de base';
+$lang['For more information on metadata visit'] = 'Pour plus d\'informations sur les métadonnées, visitez';
+$lang['Thumbnail page display'] = 'Affichage de la page des miniatures';
+$lang['Show image caption'] = 'Afficher la légende des images';
+?>
