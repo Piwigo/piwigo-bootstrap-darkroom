946cbd282434b4bfaadcde9b2e250363ad0982e9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-03-09 22:02:23 +0100

    Add sv_SE, thanks to wahlis

---
+++ language/sv_SE/theme.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2016 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Enabled'] = 'Aktiverad';
+$lang['Endlessly scroll through album images'] = 'Oändlig bläddring genom albumets bilder';
+$lang['For more information on metadata visit'] = 'För mer information om metadata besök';
+$lang['Infinite looping'] = 'Oändlig repetition';
+$lang['Link thumbnail to'] = 'Länka miniatyrbild till';
+$lang['Never'] = 'Aldrig';
+$lang['No color theme'] = 'Inget färgtema';
+$lang['A color theme from'] = 'Ett färgtema från';
+$lang['A mobile-ready theme based on Boostrap Default'] = 'Ett mobilanpassat tema baserat på Bootstrap Default';
+$lang['Always'] = 'Alltid ';
+$lang['Based on Bootstrap Default by'] = 'Baserat på Bootstrap Default från';
+$lang['Basic'] = 'Grundläggande';
+$lang['Bug reports and features requests'] = 'Felrapporter och önskemål';
+$lang['By'] = 'Från';
+$lang['Color theme'] = 'Färgtema';
+$lang['Disabled'] = 'Avstängd';
+$lang['Display a site logo image instead of plain text'] = 'Visa logo istället för text ';
+$lang['Display the currently selected image in the middle. Works best with infinite looping enabled.'] = 'Visa den valda bilden i mitten. Fungerar bäst med oändlig repetition aktiverad.';
+$lang['Enable PhotoSwipe fullscreen slideshow. Disable if you prefer to use Plugins like Fotorama or Piwigo\'s default slideshow.'] = 'Aktivera PhotoSwipe fullskärmsbildspel. Stäng av om du föredrar insticksmoduler som Fotorama eller Piwigos standard bildspel';
