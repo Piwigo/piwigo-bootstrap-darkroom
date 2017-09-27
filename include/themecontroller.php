1ae18aaf4d76e15b8eb60d733c7ee744299d21ea
Thomas Kuther <github@kuther.net>
2017-09-27 08:59:08 +0200

    waitaminute.. fix typo

--- include/themecontroller.php
+++ include/themecontroller.php
@@ -12,7 +12,7 @@ class ThemeController {
         load_language('theme.lang', PHPWG_THEMES_PATH.'bootstrap_darkroom/');
 
         add_event_handler('init', array($this, 'assignConfig'));
-        if ($this->config->bootstrap_theme === 'darkroom' || $this->config->bootstrap_theme === 'darkroom' || $this->config->bootstrap_theme === 'bootswatch') {
+        if ($this->config->bootstrap_theme === 'darkroom' || $this->config->bootstrap_theme === 'material' || $this->config->bootstrap_theme === 'bootswatch') {
           $this->config->bootstrap_theme = 'bootstrap-darkroom';
           $this->config->save();
           add_event_handler('loc_begin_page_header', array($this, 'showUpgradeWarning'));
