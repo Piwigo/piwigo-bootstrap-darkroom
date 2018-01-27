83aae596abba85f2a1194d50193b68cd7f6349b6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2018-01-27 08:36:30 +0100

    Add pl_PL, thanks to Sauron

---
+++ language/pl_PL/theme.lang.php
@@ -0,0 +1,69 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Thumbnail Link Target'] = 'Cel linku miniaturki';
+$lang['Thumbnail page display'] = 'Strona wyświetlania miniaturek';
+$lang['Social Media Integration'] = 'Integracja z mediami społecznościowymi';
+$lang['Social integration'] = 'Integracja z mediami społecznościowymi';
+$lang['Tabs below the image'] = 'Znaczniki poniżej zdjęcia';
+$lang['Show basic EXIF metadata'] = 'Pokaż podstawowe metadane EXIF';
+$lang['Show image caption'] = 'Pokaż podpis zdjęcia';
+$lang['Sidebar (like Boostrap Default)'] = 'Pasek boczny (jak w standardowym Bootstrap)';
+$lang['Simple'] = 'Prosty';
+$lang['Site logo'] = 'Logo strony';
+$lang['HTML 5 canvas'] = 'HTML 5 canvas';
+$lang['PhotoSwipe Slideshow'] = 'Pokaz zdjęć PhotoSwipe';
+$lang['Photoswipe Slideshow (Mobile devices only)'] = 'Pokaz zdjęć PhotoSwipe (tylko urządzenia mobilne)';
+$lang['Picture info display position'] = 'Miejsce wyświetlania informacji o zdjęciu';
+$lang['Picture page display'] = 'Strona wyświetlania zdjęcia';
+$lang['On mobile devices only'] = 'Tylko na urządzeniach przenośnych';
+$lang['Page header'] = 'Nagłówek strony';
+$lang['Picture details page'] = 'Strona z informacjami o zdjęciu';
+$lang['Endlessly scroll through album images'] = 'Przewijaj bez końca przez obrazki albumu';
+$lang['For more information on metadata visit'] = 'By uzyskać więcej informacji o metadanych, odwiedź';
+$lang['Link thumbnail to'] = 'Podlinkuj miniaturki do';
+$lang['List view'] = 'Widok listy';
+$lang['Never'] = 'Nigdy';
+$lang['Display category description in grid view'] = 'Wyświetl opis kategorii w postaci siatki';
+$lang['Infinite looping'] = 'Nieskończone zapętlanie';
+$lang['Integrate lower navbar'] = 'Wstaw dolny pasek nawigacji';
+$lang['Enabled'] = 'Włączony';
+$lang['Full width layout'] = 'Układ na pełnej szerokości';
+$lang['Fullscreen'] = 'Pełen ekran';
+$lang['Grid view'] = 'Widok siatki';
+$lang['Banner style'] = 'Styl baneru';
+$lang['Bug reports and feature requests'] = 'Zgłaszanie błędów i propozycji nowych funkcjonalności';
+$lang['By'] = 'Przez';
+$lang['Display number of images in album and subalbums'] = 'Wyświetl ilość obrazków w albumie i podalbumów';
+$lang['Bootstrap theme'] = 'Temat Bootstrap';
+$lang['Bootstrap\'s default theme'] = 'Domyślny temat Bootstrap';
+$lang['Category page display'] = 'Wyświetlanie strony kategorii';
+$lang['Color theme'] = 'Kolor motywu';
+$lang['Display a site logo image instead of plain text'] = 'Wyświetl logo strony, zamiast tekstu';
+$lang['Components'] = 'Komponenty';
+$lang['Description display style'] = 'Styl wyświetlania opisu';
+$lang['Disabled'] = 'Wyłączony';
+$lang['Discover'] = 'Odkryj';
+$lang['Advanced'] = 'Zaawansowane';
+$lang['Appearance'] = 'Wygląd';
+$lang['Always'] = 'Zawsze';
+$lang['Basic'] = 'Podstawowy';
