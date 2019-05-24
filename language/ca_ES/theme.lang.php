600bd63cb677e556a307afe95d89400e24f6b719
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-05-24 06:18:34 +0200

    Add ca_ES, thanks to Tirs

---
+++ language/ca_ES/theme.lang.php
@@ -0,0 +1,100 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Picture page display'] = 'Visualització de la pàgina de fotos';
+$lang['Show a jumbotron banner on the categroy pages.'] = 'Mostra una pancarta de jumbotron a la pàgina de categories.';
+$lang['Show basic EXIF metadata'] = 'Mostra les metadades EXIF bàsiques';
+$lang['Show image caption'] = 'Mostra el peu de foto';
+$lang['Sidebar (like Boostrap Default)'] = 'Barra lateral (com la predeterminada de Bootstrap)';
+$lang['Simple'] = 'Simple';
+$lang['Site logo'] = 'Logo del lloc';
+$lang['Social Media Integration'] = 'Integració social';
+$lang['Social integration'] = 'Integració social';
+$lang['Span the full viewport height'] = 'Abarca tota l\'alçada de la visualització';
+$lang['Tabs below the image'] = 'Pestanyes sota la imatge';
+$lang['The path to the image, relative to your Piwigo installation folder'] = 'Camí fins a la imatge, relatiu a la carpeta d\'instal·lació del Piwigo';
+$lang['This will display categories as media wells with squared thumbnails, similar to the smartpocket mobile theme.'] = 'Això mostrarà les categories com a pous de medis amb miniatures quadrades, de manera semblant al tema mòbil de smartpocket.';
+$lang['Thumbnail Link Target'] = 'Destinació de l\'enllaç de la miniatura';
+$lang['Thumbnail page display'] = 'Visualització de la pàgina de miniatures';
+$lang['URL or releative path to the image'] = 'URL o camí relatiu a la imatge';
+$lang['Use 6 colums for viewports >= 1680px'] = 'Utilitza 6 columnes per a visualitzacions >= 1680px';
+$lang['Use colored share buttons instead of icons'] = 'Utilitza botons de compartir en color en comptes d\'icones';
+$lang['Use description rather than title for images'] = 'Utilitza la descripció en comptes del títol per a les imatges';
+$lang['Use full width containers that span the entire width of the viewport'] = 'Utilitza contenidors amples que ocupin tot l\'ample de la visualització';
+$lang['center-aligned h5 heading'] = 'capçalera h5 centrada';
+$lang['directly in the navigation bar'] = 'directament a la barra de navegació';
+$lang['from'] = 'de';
+$lang['lazyLoad method'] = 'mètode lazyLoad';
+$lang['left-aligned free text for advanced descriptions'] = 'text lliure alineat a l\'esquerra per a les descripcions avançades';
+$lang['loads all images one after another when the page loads (use carefully!)'] = 'carrega totes les imatges una rere l\'altra quan es carrega la pàgina (utilitzeu amb compte!)';
+$lang['milliseconds'] = 'mil·lisegons';
+$lang['will load the image as soon as you slide to it'] = 'carregarà la imatge tan bon punt us desplaceu cap a ella';
+$lang['Display category description in grid view'] = 'Mostra la descripció de les categories en la vista de reixeta';
+$lang['Display number of images in album and subalbums'] = 'Mostra el nombre d\'imatges a l\'àlbum i els sub-àlbums';
+$lang['Display the currently selected image in the middle. Works best with infinite looping enabled.'] = 'Mostra la imatge seleccionada actualment al mig. Funciona millor amb el bucle infinit activat.';
+$lang['Enable PhotoSwipe fullscreen slideshow. Disable if you prefer to use Plugins like Fotorama or Piwigo\'s default slideshow.'] = 'Habilita la presentació de diapositives a pantalla completa de PhotoSwipe. Inhabiliteu-lo si preferiu utilitzar connectors com Fotorama o la presentació de diapositives per defecte de Piwigo.';
+$lang['Enable the slick carousel below the main image on the picture page'] = 'Habilita el carrusel sota la imatge principal a la pàgina de fotos';
+$lang['Enabled'] = 'Habilitat';
+$lang['Endlessly scroll through album images'] = 'Desplaçament infinit a través de les imatges de l\'àlbum';
+$lang['For more information on metadata visit'] = 'Per a més informació sobre les metadades, visiteu';
+$lang['Full width layout'] = 'Disseny d\'amplada completa';
+$lang['Fullscreen'] = 'Pantalla completa';
+$lang['Grid view'] = 'Vista de reixeta';
+$lang['HTML 5 canvas'] = 'Llenç d\'HTML 5';
+$lang['Hero image'] = 'Imatge de Hero';
+$lang['If you find any issues, please report them to the'] = 'Si trobeu qualsevol problema, comuniqueu-lo a ';
+$lang['Infinite looping'] = 'Bucle infinit';
+$lang['Integrate lower navbar'] = 'Integra la barra de navegació inferior';
+$lang['Issue tracker'] = 'Rastrejador de problemes';
+$lang['Link thumbnail to'] = 'Enllaça la miniatura a';
+$lang['List view'] = 'Vista de llista';
+$lang['Never'] = 'Mai';
+$lang['On mobile devices only'] = 'Només en dispositius mòbils';
+$lang['Page header'] = 'Capçalera de pàgina';
+$lang['PhotoSwipe Slideshow'] = 'Presentació de diapositives de PhotoSwipe';
+$lang['Photoswipe Slideshow (Mobile devices only)'] = 'Presentació de diapositives de PhotoSwipe (només dispositius mòbils)';
+$lang['Picture details page'] = 'Pàgina de detalls de la foto';
+$lang['Picture info display position'] = 'Posició de visualització de la informació de la foto';
+$lang['Display categories as Bootstrap media wells'] = 'Mostra les categories com a pous de mitjans de Bootstrap';
+$lang['Display a site logo image instead of plain text'] = 'Mostra una imatge del logo del lloc en comptes de text senzill';
+$lang['Discover'] = 'Descobreix';
+$lang['A color theme from'] = 'Un tema de color de';
+$lang['A mobile-ready theme based on Bootstrap 4'] = 'Un tema preparat per a mòbils basat en Bootstrap 4';
+$lang['Advanced'] = 'Avançat';
+$lang['Always'] = 'Sempre';
+$lang['Appearance'] = 'Apariència';
+$lang['Autoplay interval'] = 'Intèrval de reproducció automàtica';
+$lang['Banner style'] = 'Estil del banner';
+$lang['Basic'] = 'Bàsic';
+$lang['Bootstrap Darkroom\'s custom dark color theme'] = 'Tema de color fosc personalitzat de Bootstrap Darkroom';
+$lang['Bootstrap theme'] = 'Tema de Bootstrap';
+$lang['Bootstrap\'s default theme'] = 'Tema predeterminat de Bootstrap';
+$lang['Bootswatch theme'] = 'Tema de Bootswatch';
+$lang['Bug reports and feature requests'] = 'Informe d\'errors i sol·licituds de característiques';
+$lang['Center mode'] = 'Mode centrat';
+$lang['By'] = 'Per';
+$lang['Card grid below the image'] = 'Reixeta de targetes a sota de la imatge';
+$lang['Category page display'] = 'Visualització de pàgina de categories';
+$lang['Color theme'] = 'Tema de color';
+$lang['Description display style'] = 'Estil de visualització de la descripció';
+$lang['Components'] = 'Components';
+$lang['Disabled'] = 'Inhabilitat';
