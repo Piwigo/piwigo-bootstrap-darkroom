fb2f46c375cb42f6f4da61bd5ef4376e2315b59a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-05-24 06:18:45 +0200

    Add es_ES, thanks to Tirs

---
+++ language/es_ES/theme.lang.php
@@ -0,0 +1,100 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Show image caption'] = 'Mostrar el pie de foto';
+$lang['Sidebar (like Boostrap Default)'] = 'Mostrar la barra lateral (como en Bootstrap por defecto)';
+$lang['Simple'] = 'Simple';
+$lang['Site logo'] = 'Logo del sitio';
+$lang['Span the full viewport height'] = 'Abarcar toda la altura del área de visión';
+$lang['Tabs below the image'] = 'Pestañas debajo de la imagen';
+$lang['The path to the image, relative to your Piwigo installation folder'] = 'La ruta a la imagen, relativa a su carpeta de instalación de Piwigo';
+$lang['This will display categories as media wells with squared thumbnails, similar to the smartpocket mobile theme.'] = 'Esto mostrará las categorías como pozos de medios con miniaturas cuadradas, de forma similar al tema móvil de smartpocket.';
+$lang['Thumbnail Link Target'] = 'Destino del enlace de la miniatura';
+$lang['Thumbnail page display'] = 'Visualización de la página de miniaturas';
+$lang['URL or releative path to the image'] = 'URL o ruta relativa a la imagen';
+$lang['Use 6 colums for viewports >= 1680px'] = 'Utilizar 6 columnas para visualizaciones >= 1680px';
+$lang['Use colored share buttons instead of icons'] = 'Utilizar botones para compartir coloreados en lugar de iconos';
+$lang['Use description rather than title for images'] = 'Utilizar la descripción en lugar del título para las imágenes';
+$lang['Use full width containers that span the entire width of the viewport'] = 'Utilizar contenedores anchos que se extiendan por toda la amplitud de la visualización';
+$lang['center-aligned h5 heading'] = 'Cabecera h5 centrada';
+$lang['directly in the navigation bar'] = 'directamente en la barra de navegación';
+$lang['from'] = 'desde';
+$lang['lazyLoad method'] = 'método lazyLoad';
+$lang['left-aligned free text for advanced descriptions'] = 'texto libre alineado a la izquierda para las descripciones avanzadas';
+$lang['loads all images one after another when the page loads (use carefully!)'] = 'carga todas las imágenes una tras otra cuando se carga la página (¡usar con cuidado!)';
+$lang['milliseconds'] = 'milisegundos';
+$lang['will load the image as soon as you slide to it'] = 'cargará la imagen tan pronto como se deslice hasta ella';
+$lang['Picture page display'] = 'Visualización de la página de fotos';
+$lang['Show a jumbotron banner on the categroy pages.'] = 'Mostrar una pancarta de jumbotron en las páginas de categorías.';
+$lang['Show basic EXIF metadata'] = 'Mostrar metadatos EXIF básicos';
+$lang['Social integration'] = 'Integración social';
+$lang['Social Media Integration'] = 'Integración social';
+$lang['Enable the slick carousel below the main image on the picture page'] = 'Habilitar el carrusel deslizante debajo de la imagen principal en la página de fotos';
+$lang['Enabled'] = 'Habilitado';
+$lang['Endlessly scroll through album images'] = 'Desplazamiento sin fin a través de las imágenes';
+$lang['For more information on metadata visit'] = 'Para más información sobre los metadatos visita ';
+$lang['Full width layout'] = 'Diseño de ancho completo';
+$lang['Fullscreen'] = 'Pantalla completa';
+$lang['Grid view'] = 'Vista de rejilla';
+$lang['HTML 5 canvas'] = 'Lienzo de HTML 5';
+$lang['Hero image'] = 'Imagen de héroe';
+$lang['If you find any issues, please report them to the'] = 'Si encuentra algún problema, informe de él a';
+$lang['Infinite looping'] = 'Bucle infinito';
+$lang['Integrate lower navbar'] = 'Integrar la barra de navegación inferior';
+$lang['Issue tracker'] = 'Seguidor de problemas';
+$lang['Link thumbnail to'] = 'Enlazar la miniatura con';
+$lang['List view'] = 'Vista de lista';
+$lang['Never'] = 'Nunca';
+$lang['On mobile devices only'] = 'Sólo en dispositivos móviles';
+$lang['Page header'] = 'Cabecera de página';
+$lang['PhotoSwipe Slideshow'] = 'Presentación de diapositivas de PhotoSwipe';
+$lang['Photoswipe Slideshow (Mobile devices only)'] = 'Presentación de diapositivas de PhotoSwipe (sólo dispositivos móviles)';
+$lang['Picture details page'] = 'Página de detalles de la foto';
+$lang['Picture info display position'] = 'Posición de visualización de la información de la foto';
+$lang['Display category description in grid view'] = 'Visualizar las categorías en forma de rejilla';
+$lang['Display number of images in album and subalbums'] = 'Mostrar el número de imágenes en el álbum y los sub-álbumes';
+$lang['Display the currently selected image in the middle. Works best with infinite looping enabled.'] = 'Mostrar la imagen seleccionada actualmente en el centro. Funciona mejor con el bucle infinito habilitado.';
+$lang['Enable PhotoSwipe fullscreen slideshow. Disable if you prefer to use Plugins like Fotorama or Piwigo\'s default slideshow.'] = 'Habilitar la presentación de diapositivas a pantalla completa de PhotoSwipe. Inhabilítelo si prefiere utilizar plug-ins como Fotorama o la presentación de diapositivas por defecto de Piwigo.';
+$lang['Display categories as Bootstrap media wells'] = 'Visualizar las categorías como pozos de medios de Bootstrap';
+$lang['Display a site logo image instead of plain text'] = 'Visualizar una imagen de logo del sitio en lugar de texto simple';
+$lang['Discover'] = 'Descubrir';
+$lang['Description display style'] = 'Estilo de visualización de la descripción';
+$lang['Disabled'] = 'Inhabilitado';
+$lang['Components'] = 'Componentes';
+$lang['By'] = 'Por';
+$lang['Center mode'] = 'Modo de centrado';
+$lang['Color theme'] = 'Tema de color';
+$lang['Category page display'] = 'Visualización de la página de categorías';
+$lang['Card grid below the image'] = 'Rejilla de cartas bajo la imagen';
+$lang['Bug reports and feature requests'] = 'Informe de errores y peticiones de características';
+$lang['Bootswatch theme'] = 'Tema de Bootswatch';
+$lang['Bootstrap theme'] = 'Tema de Bootstrap';
+$lang['Bootstrap\'s default theme'] = 'Tema por defecto de Bootstrap';
+$lang['Bootstrap Darkroom\'s custom dark color theme'] = 'Tema de colores oscuros personalizado de Bootstrap Darkroom';
+$lang['Basic'] = 'Básico';
+$lang['Banner style'] = 'Estilo de banner';
+$lang['Autoplay interval'] = 'Intervalo de reproducción automática';
+$lang['Appearance'] = 'Apariencia';
+$lang['Advanced'] = 'Avanzado';
+$lang['Always'] = 'Siempre';
+$lang['A mobile-ready theme based on Bootstrap 4'] = 'Un tema preparado para móviles basado en Bootstrap 4';
+$lang['A color theme from'] = 'Un tema de color de';
