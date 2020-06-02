ed16f06a3dbf109b7f65416c3aaea89c1da18eb1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2020-06-02 17:43:27 +0200

    Add it_IT, thanks to virgigiole

---
+++ language/it_IT/theme.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['loads all images one after another when the page loads (use carefully!)'] = 'carica tutte le immagini una dopo l\'altra quando la pagina carica (usare attentamente!)';
+$lang['milliseconds'] = 'millisecondi';
+$lang['Link thumbnail to'] = 'Collega thumbnail a';
+$lang['Infinite looping'] = 'Loop infinito';
+$lang['If you find any issues, please report them to the'] = 'In caso di problemi, segnalali a';
+$lang['Thumbnail page display'] = 'Mostra pagina Thumbnail';
+$lang['Tabs below the image'] = 'Tabs sotto l\'immagine';
+$lang['Social integration'] = 'Integrazione social';
+$lang['Social Media Integration'] = 'Integrazione social';
+$lang['Site logo'] = 'Logo sito';
+$lang['Show basic EXIF metadata'] = 'Mostra metadati EXIF principali';
+$lang['Page header'] = 'Intestazione pagina';
+$lang['On mobile devices only'] = 'Solo su dispositivi mobili';
+$lang['Description display style'] = 'Stile di visualizzazione descrizione';
+$lang['Display category description in grid view'] = 'Mostra la descrizione della categoria in visualizzazione griglia';
+$lang['Fullscreen'] = 'Schermo intero';
+$lang['Advanced'] = 'Avanzato';
+$lang['Basic'] = 'Base';
+$lang['Display a site logo image instead of plain text'] = 'Mostra un logo del sito anziché testo';
+$lang['Display number of images in album and subalbums'] = 'Mostra il numero di immagini negli albumo e sotto-album';
+$lang['Bootstrap theme'] = 'Tema Bootstrap';
+$lang['A color theme from'] = 'Un colore tema da';
+$lang['Autoplay interval'] = 'Intervallo esecuzione automatica';
+$lang['Banner style'] = 'Stile banner';
+$lang['For more information on metadata visit'] = 'Per maggiori informazioni su metadata, visita';
+$lang['Never'] = 'Mai';
+$lang['Enabled'] = 'Abilitato';
+$lang['Use description rather than title for images'] = 'Usa descrizione piuttosto che titolo per immagini';
+$lang['Disabled'] = 'Disabilitato';
+$lang['Discover'] = 'Scopri';
+$lang['Always'] = 'Sempre';
+$lang['Color theme'] = 'Colore tema';
