cc825cdf3823f4bbc17d3c8c1393c4d276abfbb5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-09-07 18:51:09 +0200

    Update cs_CZ, thanks to jjelinek

---
+++ language/cs_CZ/theme.lang.php
@@ -0,0 +1,16 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['A color theme from'] = 'Barevné téma z';
+$lang['A mobile-ready theme based on Bootstrap 4'] = 'Téma připravené i pro mobily založené na Bootstrap 4';
+$lang['Advanced'] = 'Rozšířené';
+$lang['Always'] = 'Vždy';
+$lang['Appearance'] = 'Vzhled';
+$lang['Autoplay interval'] = 'Interval automatického přehrávání';
+$lang['Banner style'] = 'Styl banneru';
+$lang['Basic'] = 'Základní';
+$lang['Bug reports and feature requests'] = 'Hlášení chyb a požadavků na funkce';
