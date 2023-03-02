d8c0950397db0d649577b427c39633038993e511
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-02 06:42:50 +0100

    Update zh_TW, thanks to seanpai

---
+++ language/zh_TW/theme.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['A color theme from'] = '一個顏色主題來自';
+$lang['A mobile-ready theme based on Bootstrap 4'] = '一個基於Bootstrap 4並相容於手機的主題';
+$lang['Advanced'] = '進階';
+$lang['Always'] = '永遠';
+$lang['Appearance'] = '外觀';
+$lang['AutoPlay'] = '自動播放';
+$lang['Autoplay interval'] = '自動播放時長';
+$lang['Banner style'] = '橫幅樣式';
+$lang['Basic'] = '基本';
+$lang['Bootstrap Darkroom\'s custom dark color theme'] = 'Bootstrap Sarkroom的自訂暗色主題';
+$lang['Bootstrap theme'] = 'Bootstrap主題';
+$lang['Bootstrap\'s default theme'] = 'Bootstrap的預設主題';
+$lang['Bootswatch theme'] = 'Bootswatch主題';
+$lang['Bug reports and feature requests'] = '錯誤回報與功能請求';
+$lang['By'] = '來自';
+$lang['Card grid below the image'] = '圖片下方的卡片網格';
+$lang['Category page display'] = '分類頁面展示';
+$lang['Center mode'] = '置中模式';
+$lang['Close (Esc)'] = '關閉 (Esc)';
+$lang['Color theme'] = '顏色主題';
