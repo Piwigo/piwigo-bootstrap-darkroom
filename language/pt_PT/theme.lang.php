21d11aaf68b9a7b8be2f1872e81df24915cb491b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-03-05 11:54:06 +0100

    Add pt_PT, thanks to ANO

---
+++ language/pt_PT/theme.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['A color theme from'] = 'Um tema de cor de';
+$lang['A mobile-ready theme based on Bootstrap 4'] = 'Um tema pronto para dispositivos móveis baseado no Bootstrap 4';
+$lang['Advanced'] = 'Avançado';
+$lang['Always'] = 'Sempre';
+$lang['Appearance'] = 'Aparência';
+$lang['AutoPlay'] = 'Reprodução Automática';
+$lang['Autoplay interval'] = 'Intervalo de reprodução automática';
+$lang['Banner style'] = 'Estilo do banner';
+$lang['Basic'] = 'Básico';
+$lang['Bootstrap Darkroom\'s custom dark color theme'] = 'Tema de cor escura personalizado do Bootstrap Darkroom';
+$lang['Bootstrap theme'] = 'Tema Bootstrap';
+$lang['Bootstrap\'s default theme'] = 'Tema padrão do Bootstrap';
+$lang['Bootswatch theme'] = 'Tema Bootswatch';
+$lang['Bug reports and feature requests'] = 'Relatórios de bugs e solicitações de recursos';
+$lang['By'] = 'Por';
+$lang['Card grid below the image'] = 'Grade do cartão abaixo da imagem';
+$lang['Category page display'] = 'Mostrar página da categoria';
+$lang['Center mode'] = 'Modo central';
+$lang['Close (Esc)'] = 'Fechar (Esc)';
+$lang['Color theme'] = 'Cor do tema';
