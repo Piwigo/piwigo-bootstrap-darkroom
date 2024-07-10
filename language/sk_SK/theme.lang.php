f06d6409473e208339feb6122917f669c8317434
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-07-10 06:44:05 +0200

    Update sk_SK, thanks to jose1711

---
+++ language/sk_SK/theme.lang.php
@@ -0,0 +1,71 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Zoom in/out'] = 'Priblížiť/Oddialiť';
+$lang['from'] = 'z';
+$lang['Tabs below the image'] = 'Karty pod obrázkom';
+$lang['The path to the image, relative to your Piwigo installation folder'] = 'Cesta k obrázku, vzhľadom na váš priečinok s inštaláciou Piwigo';
+$lang['Toggle fullscreen'] = 'Prepnúť zobrazenie na celú obrazovku';
+$lang['URL or releative path to the image'] = 'URL adresa alebo relatívna cesta k obrázku';
+$lang['Use colored share buttons instead of icons'] = 'Používať farebné tlačidlá zdieľania miesto ikon';
+$lang['Use description rather than title for images'] = 'Používať popisy miesto názvov obrázkov';
+$lang['Picture details page'] = 'Stránka s podrobnosťami o obrázku';
+$lang['Picture info display position'] = 'Umiestnenie informácií o obrázku';
+$lang['Picture page display'] = 'Zobrazenie stránky s obrázkom';
+$lang['Previous (arrow left)'] = 'Predchádzajúci (šípka vľavo)';
+$lang['Share'] = 'Zdieľať';
+$lang['Show basic EXIF metadata'] = 'Zobraziť základné informácie EXIF';
+$lang['Show image caption'] = 'Zobraziť popis obrázka';
+$lang['Sidebar (like Boostrap Default)'] = 'Bočná lista (predvolený Bootstrap)';
+$lang['Simple'] = 'Jednoduché';
+$lang['Site logo'] = 'Logo webstránky';
+$lang['HTML 5 canvas'] = 'Plátno HTML 5';
+$lang['If you find any issues, please report them to the'] = 'Ak nájdete akýkoľvek problém, nahláste ho prosím na';
+$lang['Infinite looping'] = 'Nekonečná slučka';
+$lang['Integrate lower navbar'] = 'Integrovať spodnú navigačnú lištu';
+$lang['Issue tracker'] = 'Hlásenie chýb';
+$lang['Link thumbnail to'] = 'Prepojiť náhľad s';
+$lang['List view'] = 'Zobrazenie zoznamu';
+$lang['Never'] = 'Nikdy';
+$lang['Next (arrow right)'] = 'Ďalej (šípka vpravo)';
+$lang['On mobile devices only'] = 'Iba na mobilných zariadeniach';
+$lang['Page header'] = 'Hlavička stránky';
+$lang['PhotoSwipe Slideshow'] = 'Prezentácia PhotoSwipe';
+$lang['Photoswipe Slideshow (Mobile devices only)'] = 'Prezentácia PhotoSwipe (iba mobilné zariadenia)';
+$lang['Picture details'] = 'Podrobnosti obrázka';
+$lang['Card grid below the image'] = 'Mriežka s kartami pod obrázkom';
+$lang['Components'] = 'Súčasti';
+$lang['Description display style'] = 'Štýl zobrazenia popisu';
+$lang['Disabled'] = 'Vypnuté';
+$lang['Discover'] = 'Objavte';
+$lang['Display a site logo image instead of plain text'] = 'Zobraziť obrázok loga webstránky miesto obyčajného textu';
+$lang['Display category description in grid view'] = 'Zobraziť popis kategórie v zobrazení mriežky';
+$lang['Display number of images in album and subalbums'] = 'Zobraziť počet obrázkov a albumoch a podalbumoch';
+$lang['Display the currently selected image in the middle. Works best with infinite looping enabled.'] = 'Zobraziť aktuálne vybraný obrázok v strede. Najlepšie funguje pri povolení nekonečného skrolovania.';
+$lang['Enabled'] = 'Povolené';
+$lang['Endlessly scroll through album images'] = 'Nekonečné skrolovanie obrázkov v albume';
+$lang['For more information on metadata visit'] = 'Pre viac informácií o metadátach navštívte';
+$lang['Full width layout'] = 'Rozloženie na celú šírku';
+$lang['Fullscreen'] = 'Celá obrazovka';
+$lang['Grid view'] = 'Zobrazenie mriežky';
+$lang['A color theme from'] = 'Farebný motív z';
+$lang['A mobile-ready theme based on Bootstrap 4'] = 'Motív vhodný aj pre mobilné zariadenia postavený na Bootstrap 4';
+$lang['Advanced'] = 'Pokročilé';
+$lang['Always'] = 'Vždy';
+$lang['Appearance'] = 'Vzhľad';
+$lang['AutoPlay'] = 'Automatické prehrávanie';
+$lang['Autoplay interval'] = 'Interval pre autom. prehrávanie';
+$lang['Basic'] = 'Základný';
+$lang['Bootstrap theme'] = 'Motív Bootstrap';
+$lang['Bootstrap\'s default theme'] = 'Predvolený motív Bootstrap';
+$lang['Bootswatch theme'] = 'Motív Bootswatch';
+$lang['Bug reports and feature requests'] = 'Hlásenia chýb a žiadosti o pridanie funkcie';
+$lang['By'] = 'Od';
+$lang['Category page display'] = 'Zobrazenie stránky s kategóriami';
+$lang['Center mode'] = 'Režim vystredenia';
+$lang['Close (Esc)'] = 'Zavrieť (Esc)';
+$lang['Color theme'] = 'Farebný motív';
