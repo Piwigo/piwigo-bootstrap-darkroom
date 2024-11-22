792edcd4a6f53b95d1c876ac0ac800f8f1c2ac19
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-22 06:15:03 +0100

    Update et_EE, thanks to Exabot

---
+++ language/et_EE/theme.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Color theme'] = 'Värvi teema';
+$lang['Close (Esc)'] = 'Sule (Esc)';
+$lang['Card grid below the image'] = 'Pildi all olev kaardiruudustik';
+$lang['Category page display'] = 'Kategooria lehe kuvamine';
+$lang['Center mode'] = 'Keskmine režiim';
+$lang['By'] = 'Autor';
+$lang['Bug reports and feature requests'] = 'Veaaruanded ja funktsioonitaotlused';
+$lang['Bootswatch theme'] = 'Bootswatchi teema';
+$lang['Bootstrap\'s default theme'] = 'Bootstrapi vaiketeema';
+$lang['Bootstrap theme'] = 'Bootstrapi teema';
+$lang['Bootstrap Darkroom\'s custom dark color theme'] = 'Bootstrap Darkroomi kohandatud tumedate värvide teema';
+$lang['A color theme from'] = 'Värviteema alates';
+$lang['A mobile-ready theme based on Bootstrap 4'] = 'Mobiili jaoks sobiv teema, mis põhineb Bootstrap 4-l';
+$lang['Basic'] = 'Põhiline';
+$lang['Banner style'] = 'Bänneri stiil';
+$lang['Autoplay interval'] = 'Automaatesituse intervall';
+$lang['AutoPlay'] = 'AutoPlay';
+$lang['Appearance'] = 'Välimus';
+$lang['Always'] = 'Alati';
+$lang['Advanced'] = 'Täpsemalt';
